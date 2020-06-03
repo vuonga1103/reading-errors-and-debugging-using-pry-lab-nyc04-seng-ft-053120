132c98f5074855d0a2c5508f6a13df7190f8a4a7
Anh Vuong <vuonga1103@yahoo.com>
2020-06-03 16:25:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,14 +1,10 @@
 # don't forget to add: require 'pry'
-require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
-    # "s" * 10 + string
-    binding.pry
+    "s" * 10 + string
   else
     string
-    binding.pry
   end
 end
 
